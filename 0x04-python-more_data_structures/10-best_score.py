71949303e531f20cacdef76e4dc55cc0bf56cb93
Chukwuderah <pleasantchukwu@gmail.com>
2023-11-08 12:54:01 -0800

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary
-    return None
-
-Maxi = 0
-for key, value in a_dictionary.items():
-    if value > Maxi:
-        maxi = value
-
+    if not a_dictionary:
+        return None
+    Maxi = 0
     for key, value in a_dictionary.items():
-        if value = Maxi:
+        if value > Maxi:
+            Maxi = value
+    for key, value in a_dictionary.items():
+        if value == Maxi:
             return key
