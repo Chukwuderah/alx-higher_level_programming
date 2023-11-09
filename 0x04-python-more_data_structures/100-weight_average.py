10529eaa4650a5d520dc032ee2ef4d126fd6df6c
Chukwuderah <pleasantchukwu@gmail.com>
2023-11-08 16:34:20 -0800

    13. Weighted average!

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    average = 0
+    d = 0
+    for tup in my_list:
+        average += tup[0] * tup[1]
+        d += tup[1]
+    return float(average / d)
